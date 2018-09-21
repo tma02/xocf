a87dc1aad9f1c407ca455286c5877c587064be29
Tianyi Ma <tn.ma@outlook.com>
2018-09-21 16:09:27 -0500

    Center app

--- public/app.js
+++ public/app.js
@@ -10,10 +10,23 @@ var app = new Vue({
 
 var worker = new Worker('worker.js')
 
+function centerApp() {
+  var top = (window.innerHeight / 2) - (document.getElementById('app').offsetHeight / 2)
+  console.log(top)
+  if (top < 24) {
+    top = 24
+  }
+  document.getElementById('app').style.marginTop = `${top}px`
+}
+
 worker.onmessage = (e) => {
   app.usage = e.data.data
   document.title = e.data.data.title
+
+  app.$nextTick(centerApp)
 }
+
+window.onresize = centerApp
 
 window.onload = () => {
   worker.postMessage({ message: 'start' })
